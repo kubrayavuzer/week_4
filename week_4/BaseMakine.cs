d66af2920a17b0a689d7918caf861ac8971c3720
kubra <kubra@DESKTOP-K1F6O3C>
2024-08-20 00:06:34 +0300

    Proje dosyası ekle.

---
+++ week_4/BaseMakine.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week4_closing
+{
+    abstract class BaseMakine
+    {
+        public string UretimTarihi { get; private set; }
+        public string SeriNumarasi { get; set; }
+        public string Ad { get; set; }
+        public string Aciklama { get; set; }
+        public string IsletimSistemi { get; set; }
+
+        public BaseMakine()
+        {
+            UretimTarihi = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");  // Üretim Tarihi otomatik olarak atanıyor
+
+
+        }
+
+        public virtual void BilgileriYazdir()
+        {
+            Console.WriteLine($"Üretim tarihi: {UretimTarihi}");
+            Console.WriteLine($"Seri numarası: {SeriNumarasi}");
+            Console.WriteLine($"Ad: {Ad}");
+            Console.WriteLine($"Açıklama: {Aciklama}");
+            Console.WriteLine($"İşletim sistemi: {IsletimSistemi}");
+        }
+
+        public abstract string UrunAdiGetir();
+
+    }
+
+    class Telefon : BaseMakine
+    {
+        public bool TrLisansli { get; set; }
+
+        public override void BilgileriYazdir()
+        {
+            base.BilgileriYazdir();
+            Console.WriteLine($"TR Lisanslı: {(TrLisansli ? "Evet" : "Hayır")}");
+
+        }
+
+        public override string UrunAdiGetir()
+        {
+            return $"Telefon adı: {Ad}";
+
+        }
+
+        class Bilgisayar : BaseMakine
+        {
+            private int _usbGirisSayisi;
+
+            public int UsbGirisSayisi
+            {
+                get { return _usbGirisSayisi; }
+                set
+                {
+                    if (value == 2 || value == 4)
+                    {
+                        _usbGirisSayisi = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Usb giriş sayısı 2 veya 4 olmalıdır.");
+                        _usbGirisSayisi = -1;
+                    }
+                }
+            }
+            public bool Bluetooth { get; set; }
+
+            public override void BilgileriYazdir()
+            {
+                base.BilgileriYazdir();
+
+                if (_usbGirisSayisi != -1)
+                {
+                    Console.WriteLine($"USB Giriş Sayısı: {UsbGirisSayisi}");
+                }
+
+                Console.WriteLine($"Bluetooth: {(Bluetooth ? "Evet" : "Hayır")}");
+            }
+
+            public override string UrunAdiGetir()
+            {
+                return $"Bilgisayarınızın adı ---> {Ad}";
+            }
+        }
+    }
+
+
+}
---
+++ week_4/Program.cs
@@ -0,0 +1,78 @@
+﻿using week4_closing;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Telefon üretmek için 1e, bilgisayar için 2ye basın.");
+            string secim = Console.ReadLine();
+
+            if (secim == "1")
+            {
+                Telefon telefon = new Telefon();
+
+                Console.Write("Seri noı: ");
+                telefon.SeriNumarasi = Console.ReadLine();
+
+                Console.Write("Ad: ");
+                telefon.Ad = Console.ReadLine();
+
+                Console.Write("Açıklama: ");
+                telefon.Aciklama = Console.ReadLine();
+
+                Console.Write("İşletim Sistemi: ");
+                telefon.IsletimSistemi = Console.ReadLine();
+
+                Console.Write("Tr Lisanslı mı? (Evet/Hayır): ");
+                string trLisansli = Console.ReadLine();
+                telefon.TrLisansli = trLisansli.ToLower() == "evet";
+
+                Console.WriteLine("\nÜrün üretildi.");
+                telefon.BilgileriYazdir();
+                Console.WriteLine(telefon.UrunAdiGetir());
+            }
+            else if (secim == "2")
+            {
+                Bilgisayar bilgisayar = new Bilgisayar();
+
+                Console.Write("Seri no: ");
+                bilgisayar.SeriNumarasi = Console.ReadLine();
+
+                Console.Write("Ad: ");
+                bilgisayar.Ad = Console.ReadLine();
+
+                Console.Write("Açıklama: ");
+                bilgisayar.Aciklama = Console.ReadLine();
+
+                Console.Write("İşletim Sistemi: ");
+                bilgisayar.IsletimSistemi = Console.ReadLine();
+
+                Console.Write("USB Giriş Sayısı 2 veya 4: ");
+                bilgisayar.UsbGirisSayisi = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Bluetooth var mı? (Evet/Hayır): ");
+                string bluetooth = Console.ReadLine();
+                bilgisayar.Bluetooth = bluetooth.ToLower() == "evet";
+
+                Console.WriteLine("\nÜrün başarıyla üretildi.");
+                bilgisayar.BilgileriYazdir();
+                Console.WriteLine(bilgisayar.UrunAdiGetir());
+            }
+            else
+            {
+                Console.WriteLine("Lütfen tekrar deneyin.");
+                continue;
+            }
+
+            Console.WriteLine("\nBaşka ürün istiyor musunuz? (Evet/Hayır)");
+            string devam = Console.ReadLine();
+            if (devam.ToLower() != "evet")
+            {
+                Console.WriteLine("Başarılı");
+                break;
+            }
+        }
+    }
+}
